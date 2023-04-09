45a8c286b108cabefafdac66c66a5fb45d33cf53
KeremTancan <tancankerem149@gmail.com>
2023-04-09 16:34:49 +0300

    panel

--- Assets/Scripts/PanelOpener.cs
+++ Assets/Scripts/PanelOpener.cs
@@ -21,16 +21,41 @@ public class PanelOpener : MonoBehaviour
 
     public void ScoreIncrease()
     {
-        if (ScoreCounter.scoreValue >= 30)
+       
+        
+        switch(ScoreCounter.scoreValue)
         {
-            nefesBar.SetMaxBreath(60);
-            nefesBar.SetBreath(60);
-            NefesBar.maxBreath = 60;
-            NefesBar.currentBreath = 60;
-            ScoreCounter.scoreValue = 0;
+            case >= 200:
+                Debug.Log("nefes 120");
+                nefesBar.SetMaxBreath(120);
+                nefesBar.SetBreath(120);
+                NefesBar.maxBreath = 120;
+                NefesBar.currentBreath = 120;
+                ScoreCounter.scoreValue -= 200;
+                
+                break;
 
+            case >=90:
+                Debug.Log("nefes 60");
+                nefesBar.SetMaxBreath(60);
+                nefesBar.SetBreath(60);
+                NefesBar.maxBreath = 60;
+                NefesBar.currentBreath = 60;
+                ScoreCounter.scoreValue -= 90;
+                break;
+            case >=30:
+                Debug.Log("nefes 45");
+                nefesBar.SetMaxBreath(45);
+                nefesBar.SetBreath(45);
+                NefesBar.maxBreath = 45;
+                NefesBar.currentBreath = 45;
+                ScoreCounter.scoreValue -= 30;
+                break;
         }
+        
+        
 
+        
     }
 
 
