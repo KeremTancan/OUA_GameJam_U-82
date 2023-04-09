5bbe02ccb7ae9e509e39b2a4e42c31c81b7c926b
Umut Can GÜR <umutcangurr@gmail.com>
2023-04-09 17:45:27 +0300

    A

---
+++ Assets/ScriptsA/NextButton.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextButton : MonoBehaviour
+{
+   
+    public void Next1()
+    {
+        SceneManager.LoadScene("Ardahan"); // Sonraki sahnenin adýný koy
+
+    }
+
+    public void Next2()
+    {
+        SceneManager.LoadScene("Ardahan"); // Sonraki sahnenin adýný koy
+
+    }
+
+    public void Next3()
+    {
+        SceneManager.LoadScene("Ardahan"); // Sonraki sahnenin adýný koy
+
+    }
+
+    public void Next4()
+    {
+        SceneManager.LoadScene("Ardahan"); // Sonraki sahnenin adýný koy
+
+    }
+
+}
